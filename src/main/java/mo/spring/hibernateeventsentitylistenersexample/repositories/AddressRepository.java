3e04f9cf4487e7b77860eb9c8387ebac48f7a05a
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2022-01-14 15:28:59 +0100

    add repositories

---
+++ src/main/java/mo/spring/hibernateeventsentitylistenersexample/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package mo.spring.hibernateeventsentitylistenersexample.repositories;
+
+import mo.spring.hibernateeventsentitylistenersexample.entities.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
