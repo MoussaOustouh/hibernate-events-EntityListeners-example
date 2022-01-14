a575f935e57cb1e22176e4b9784514674c4e5b08
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2022-01-14 15:16:58 +0100

    first commit

---
+++ src/main/java/mo/spring/hibernateeventsentitylistenersexample/HibernateEventsEntityListenersExampleApplication.java
@@ -0,0 +1,13 @@
+package mo.spring.hibernateeventsentitylistenersexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateEventsEntityListenersExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateEventsEntityListenersExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mo/spring/hibernateeventsentitylistenersexample/HibernateEventsEntityListenersExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package mo.spring.hibernateeventsentitylistenersexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateEventsEntityListenersExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
